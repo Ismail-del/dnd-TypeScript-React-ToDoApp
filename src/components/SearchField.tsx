deb83b7f5eb5767be6ab3ca1678912a78e102069
isaghraoui <isaghraoui@positivethinking.tech>
2022-02-24 09:42:50 +0100

    adding useForm section

---
+++ src/components/SearchField.tsx
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+
+const TitleField = () => {
+    return <Fragment>
+        {/* <section>
+            <label>Search Bar</label>
+            <Controller
+                render={({ field }) => <TextField {...field} />}
+                name="TextField"
+                control={control}
+            />
+        </section> */}
+    </Fragment>
+}
+
+export default TitleField;
